38752da35c1f682442e8a3d06074014d37fb47db
Troy Reyles <troyreyles@gmail.com>
2018-05-10 04:56:57 -0700

    hero styling  and breakpoints

--- src/main.js
+++ src/main.js
@@ -1,16 +1,10 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
-    // anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
-    // continuousVertical: true,
-    // sectionsColor: ['#fff', '#D5D5D5', '#D5D5D5', '#D5D5D5'],
+    anchors:['home', 'cinefile', 'polytone', 'css-studies'],
+    lockAnchors: true,
+    continuousVertical: true,
     scrollingSpeed: 400,
-    navigation: true
-    // onLeave: function(index, nextIndex, direction) {
-    //   var leavingSection = $(this);
-
-    //   if(index == 2 && direction == 'down') {
-    //     document.querySelector('body').classList.add('gray')
-    //   }
-    // }
+    navigation: true,
+    verticalCentered: false
   });
 });

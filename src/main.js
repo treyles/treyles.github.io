47a36f285effe183fe0d5be78c649641d5fac187
Troy Reyles <troyreyles@gmail.com>
2018-05-12 04:33:44 -0700

    added svg scroll hint, footer transitions

--- src/main.js
+++ src/main.js
@@ -1,10 +1,40 @@
-$(document).ready(function() {
+$(function() {
+  var slides = ['home', 'cinefile', 'polytone', 'css-studies'];
+
+  function loadSlide(anchor) {
+    var selection = '.' + anchor;
+    $(selection).addClass('show');
+
+    if (anchor !== 'home') {
+      $('.view-project')
+        .addClass('show')
+        .attr('href', 'src/' + anchor + '.html');
+    }
+  }
+
+  function leaveSlide(index, nextIndex) {
+    if (nextIndex === 1) {
+      // remove arrow hint
+      $('.gray-arrow').css({
+        display: 'none'
+      });
+
+      $('.view-project').removeClass('show');
+    }
+
+    // clear titles
+    $.each(slides, function(index, value) {
+      var selection = '.' + value;
+      $(selection).removeClass('show');
+    });
+  }
+
   $('#fullpage').fullpage({
-    anchors:['home', 'cinefile', 'polytone', 'css-studies'],
+    anchors: slides,
+    afterLoad: loadSlide,
+    onLeave: leaveSlide,
     lockAnchors: true,
     continuousVertical: true,
-    scrollingSpeed: 400,
-    navigation: true,
     verticalCentered: false
   });
 });

fd526218104dcaa46a15c1e81ab9270163cc4183
Troy Reyles <troyreyles@gmail.com>
2018-05-30 20:56:18 -0700

    finished page transitions, header scroll

--- src/main.js
+++ src/main.js
@@ -146,6 +146,43 @@
 
 
 
+// hide on scroll jquery script modified from:
+// http://jsfiddle.net/mariusc23/s6mLJ/31/
+var didScroll;
+var lastScrollTop = 0;
+
+window.onscroll = function(event) {
+  didScroll = true;
+}
+
+setInterval(function() {
+  if (didScroll) {
+    hasScrolled();
+    didScroll = false;
+  }
+}, 250)
+
+function hasScrolled() {
+  var header = document.querySelector('header');
+  var winHeight = window.innerHeight;
+  var docHeight = document.body.clientHeight;
+  var scrollTop = Math.max(window.pageYOffset, 
+    document.documentElement.scrollTop, document.body.scrollTop)
+
+  console.log(winHeight)
+  console.log(docHeight)
+  console.log(scrollTop)
+
+  if (scrollTop > lastScrollTop) {
+    header.style.opacity = '0';
+  } else if (scrollTop + winHeight < docHeight) {
+    header.style.opacity = '1';
+  }
+  
+  lastScrollTop = scrollTop;
+}
+
+
 
 
 
@@ -153,7 +190,28 @@
 
 
 window.onload = introAnimations;
+
 TweenLite.defaultEase = Power3.easeOut;
+// TweenLite.defaultEase = CustomEase.create("custom", "M0,0 C0.11,0.494 0.187,0.694 0.29,0.79 0.4,0.892 0.504,1 1,1");
+var titleEase = CustomEase.create("custom", "M0,0 C0.12,0.664 0.456,0.401 0.614,0.55 0.772,0.726 0.602,0.942 1,1");
+
+var introHome = new TimelineLite({paused: true});
+var introProject = new TimelineLite({paused: true})
+
+introHome
+  .to('.blackbox', .8, {y: '-100%', delay: 1})
+  .set('.blackbox', {alpha: 0})
+  .to('.content', 1.5, {y: 0}, '-=.6')
+  .to('h4', .8, {opacity: 1, ease: Power0.easeNone}, '-=.4')
+  .eventCallback('onComplete', enableHover);
+
+
+introProject
+  .to('.load-project', 2.5, {bottom: '100%', delay: .5, ease: titleEase})
+  .to('.blackbox', .8, {y: '-100%'}, '-=.6')
+  .set('.blackbox', {alpha: 0})
+  .fromTo('h2', 1.6, {alpha: 0, y: '110'}, {alpha: 1, y: '0'}, '-=.6')
+  .fromTo('.hashtags', 1, {alpha: 0}, {alpha: 1}, '-=.6');
 
 function introAnimations() {
   var page = document.body.getAttribute('data-home');
@@ -164,20 +222,6 @@ function introAnimations() {
     introProject.play();
   }
 }
-
-var introHome = new TimelineLite({paused: true})
-  .to('.blackbox', .8, {y: '-100%', delay: 1})
-  .set('.blackbox', {alpha: 0})
-  .to('.content', 1.5, {y: 0}, '-=.6')
-  .to('h4', .8, {opacity: 1, ease: Power0.easeNone}, '-=.4')
-  .eventCallback('onComplete', enableHover);
-
-
-var introProject = new TimelineLite({paused: true})
-  .to('.load-project', 1.5, {bottom: 0, delay: .5})
-  .to('.load-project', 1, {bottom: '100%', delay: 1})
-  .to('.blackbox', .8, {y: '-100%'}, '-=.6')
-  .set('.blackbox', {alpha: 0})
 
 var hoverSections = document.querySelectorAll('.home')
 hoverSections.forEach(function(section) {
@@ -235,8 +279,12 @@ function handleInternalLink(e) {
   e.preventDefault();
   var link = e.target.href;
 
- TweenLite.fromTo('.blackbox', .8, {y: '-100%', alpha: 1}, {y: '0%'})
-  .eventCallback('onComplete', function() {
-    window.location.href = link;
-  });
+  setTimeout(go, 400);
+
+  function go() {
+    TweenLite.fromTo('.blackbox', .8, {y: '-100%', alpha: 1}, {y: '0%'})
+    .eventCallback('onComplete', function() {
+      window.location.href = link;
+    });
+  }
 }
